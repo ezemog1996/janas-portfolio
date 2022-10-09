9cf5583e20ab8c69acba5ad9d90a8be4a0676213
ezemog1996 <enrique_g_gomez@hotmail.com>
2022-10-08 21:25:57 -0400

    fixed profile

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -4,8 +4,8 @@ function Contact() {
     return (
         <div className='row contact d-flex justify-content-center p-5 mx-auto'>
             <h1 className='col-12 d-flex justify-content-center'>Contact Me <i className="fa-solid fa-heart"></i></h1>
-            <h2 className='col-12 d-flex justify-content-center'><strong>janagomez019@gmail</strong></h2>
-            <h2 className='col-12 d-flex justify-content-center'><strong>(949) 400-2637</strong></h2>
+            <h2 className='col-12 d-flex justify-content-center'><strong><a href='mailto:janagomez019@gmail.com'>janagomez019@gmail.com</a></strong></h2>
+            <h2 className='col-12 d-flex justify-content-center'><strong><a href='tel:9494002637'>(949) 400-2637</a></strong></h2>
         </div>
     )
 }
